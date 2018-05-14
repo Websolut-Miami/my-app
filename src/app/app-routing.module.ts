2a743e8b52b15ea30b033d4730edde0ab9d4cb25
Gatto Digital <andy@gattodigital.com>
2018-05-13 22:36:07 -0500

    ...
    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ServersComponent } from './servers/servers.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { PracticeDirectivesComponent } from './practice-directives/practice-directives.component';
 
 const routes: Routes = [
   { path: 'servers', component: ServersComponent },
   { path: 'databinding', component: DatabindingComponent }
+  { path: 'practice-directives', component: PracticeDirectivesComponent }
 ];
 
 @NgModule({
